1f8777da77fe073d0fc4e3f7fdf5e0f0bd14fcaf
Dwlad90 <dwlad90@gmail.com>
2024-09-14 13:16:56 +0300

    compile with swc

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const stylexPlugin = require("@stylexswc/nextjs-plugin");
+const withMDX = require("@next/mdx")();
+
+
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  transpilePackages: ["@stylexjs/open-props", '@bmcwebdev/tokens'],
+  // Optionally, add any other Next.js config below
+  swcMinify: true,
+  experimental: {
+    mdxRs: true,
+    swcPlugins: [[
+      "@stylexswc/swc-plugin",
+      {
+        "dev": false,
+        "runtimeInjection": false,
+        "genConditionalClasses": true,
+        "treeshakeCompensation": true,
+        "unstable_moduleResolution": {
+          "type": "commonJS",
+          "rootDir": __dirname
+        }
+      }
+    ]],
+  },
+};
+
+module.exports = stylexPlugin({
+  rootDir: __dirname,
+})(withMDX(nextConfig));
